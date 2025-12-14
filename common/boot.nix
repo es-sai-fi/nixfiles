1aa0d53809b20002931a69a34680a6abb94bd457
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-14 01:37:39 -0500

    feat: set systemd-boot.editor to false and add kernel param

--- common/boot.nix
+++ common/boot.nix
@@ -1,7 +1,11 @@
 {pkgs}: {
   boot = {
+    kernelParams = ["boot.shell_on_fail"];
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        editor = false;
+      };
       efi.canTouchEfiVariables = true;
     };
     tmp.cleanOnBoot = true;
