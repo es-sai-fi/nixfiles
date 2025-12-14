1d4ceb4c90f25ee588008ecdbb9ab7e17d851ce0
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-13 21:03:26 -0500

    feat: declare root user password with hashedPasswordFile?

--- common/users.nix
+++ common/users.nix
@@ -1,7 +1,10 @@
 {
   users.mutableUsers = false;
+  users.users.root = {
+    hashedPasswordFile = ../secrets/root_password.txt;
+  };
   users.users.es-sai-fi = {
-    hashedPasswordFile = ../secrets/password.txt;
+    hashedPasswordFile = ../secrets/es-sai-fi_password.txt;
     isNormalUser = true;
     extraGroups = [
       "networkmanager"
