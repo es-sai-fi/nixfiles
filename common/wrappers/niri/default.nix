3d890e6a9336373ecaebc52e1ec2c6cca49732f7
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-16 17:59:02 -0500

    fix: wrap both niri bins

--- common/wrappers/niri/default.nix
+++ common/wrappers/niri/default.nix
@@ -18,6 +18,9 @@ pkgs.symlinkJoin {
     mkdir -p $out/niri
     cp ${./config.kdl} $out/niri/config.kdl
 
+    wrapProgram $out/bin/niri-session \
+      --set NIRI_CONFIG $out/niri/config.kdl
+
     wrapProgram $out/bin/niri \
       --set NIRI_CONFIG $out/niri/config.kdl
   '';
