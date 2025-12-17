e720c84a21268b4e6b41a286bd6444d88d548078
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-17 00:15:56 -0500

    feat: impermanence finally setted up

--- common/impermanence.nix
+++ common/impermanence.nix
@@ -4,51 +4,39 @@
 }: {
   imports = [impermanenceModule];
 
-  # Taken from raf
-  boot.initrd.systemd = {
-    enable = true;
-    services.rollback = {
-      description = "Rollback BTRFS root subvolume to a pristine state";
-      wantedBy = ["initrd.target"];
+  boot.initrd.postDeviceCommands = lib.mkBefore ''
+    mkdir -p /mnt
 
-      before = ["sysroot.mount"];
+    # We first mount the BTRFS root to /mnt
+    # so we can manipulate btrfs subvolumes.
+    mount -t btrfs -o subvol=/ /dev/disk/by-label/nixos /mnt
 
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig.Type = "oneshot";
-      script = ''
-        mkdir -p /mnt
+    # While we're tempted to just delete /root and create
+    # a new snapshot from /root-blank, /root is already
+    # populated at this point with a number of subvolumes,
+    # which makes `btrfs subvolume delete` fail.
+    # So, we remove them first.
+    #
+    # /root contains subvolumes:
+    # - /root/var/lib/portables
+    # - /root/var/lib/machines
 
-        # We first mount the BTRFS root to /mnt
-        # so we can manipulate btrfs subvolumes.
-        mount -o subvol=/ /dev/disk/by-label/nixos /mnt
+    btrfs subvolume list -o /mnt/root |
+      cut -f9 -d' ' |
+      while read subvolume; do
+        echo "deleting /$subvolume subvolume..."
+        btrfs subvolume delete "/mnt/$subvolume"
+      done &&
+      echo "deleting /root subvolume..." &&
+      btrfs subvolume delete /mnt/root
+    echo "restoring blank /root subvolume..."
+    btrfs subvolume snapshot /mnt/root-blank /mnt/root
 
-        # While we're tempted to just delete /root and create
-        # a new snapshot from /root-blank, /root is already
-        # populated at this point with a number of subvolumes,
-        # which makes `btrfs subvolume delete` fail.
-        # So, we remove them first.
-        #
-        # /root contains subvolumes:
-        # - /root/var/lib/portables
-        # - /root/var/lib/machines
-
-        btrfs subvolume list -o /mnt/root |
-          cut -f9 -d' ' |
-          while read subvolume; do
-            echo "deleting /$subvolume subvolume..."
-            btrfs subvolume delete "/mnt/$subvolume"
-          done &&
-          echo "deleting /root subvolume..." &&
-          btrfs subvolume delete /mnt/root
-        echo "restoring blank /root subvolume..."
-        btrfs subvolume snapshot /mnt/root-blank /mnt/root
-
-        # Once we're done rolling back to a blank snapshot,
-        # we can unmount /mnt and continue on the boot process.
-        umount /mnt
-      '';
-    };
-  };
+    # Once we're done rolling back to a blank snapshot,
+    # we can unmount /mnt and continue on the boot process.
+    umount /mnt
+    echo "disk unmounted"
+  '';
 
   environment.persistence."/persist" = {
     hideMounts = true;
