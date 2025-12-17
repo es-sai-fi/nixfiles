c70bfc64f1245b9d2e8aec73bfa8c617608a2c3b
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-16 23:01:38 -0500

    fix: mounting wrong vol for /

--- hosts/seer/hardware-configuration.nix
+++ hosts/seer/hardware-configuration.nix
@@ -20,7 +20,7 @@
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "btrfs";
-    options = ["subvol=nix" "compress=zstd" "ssd" "noatime"];
+    options = ["subvol=root" "compress=zstd" "ssd" "noatime"];
   };
 
   fileSystems."/nix" = {
